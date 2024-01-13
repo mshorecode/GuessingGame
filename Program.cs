29b13460031f7dea7feed103589aa14f27282259
Ryan Shore <mshorecode@gmail.com>
2024-01-13 02:16:51 -0600

    start of phase six logic

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿string greeting = "\n\t\tWelcome to Guessing Game!\n\tYou have 4 guesses to get the secret number!";
 bool userGuess;
-int numericValue;
+int numericValue = 0;
 Random random = new Random();
 int secretNumber = random.Next(1, 101);
 
@@ -27,3 +27,14 @@ for (int i = 3; i >= 0; i--)
         Console.WriteLine("\nSorry that's not the secret number. Try Again");
     }
 }
+
+
+int HigherOrLower(int numericValue, int secretNumber)
+{
+    // Compare numericValue to secretNumber and return a value 
+    // Possibly subtraction or bool
+    return numericValue - secretNumber;
+}
+
+int difference = HigherOrLower(numericValue, secretNumber);
+Console.WriteLine(difference);
